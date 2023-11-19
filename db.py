d438eee0682c978ab8c41a26dbd7de019bc00ac8
3TTemi <temi.btw@gmail.com>
2023-11-20 02:17:32 +0400

    Added Game and User Tables and established their relationships

---
+++ db.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+game_user_association = db.Table("game_user_association", db.Model.metadata,
+    db.Column("game_id", db.Integer, db.ForeignKey("game.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
+    )
+
+class Game(db.Model):
+    """
+    Game Model 
+    """
+    _tablename__ = "game"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sport = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    date_time = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.Stirng, nullable=False)
+    """
+    PickleType allows us to use tuple as dattpe (automatically serializes and deserializaes for databse) 
+    """
+    teams = db.Column(db.PickleType)
+    num_tickets = db.Column(db.Integer, nullable=False)
+    # Link to the User tbale using association table 
+    users_attending = db.relationship("User", secondary=game_user_association, back_populates="past_games")
+
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Game Object 
+        """
+        self.sport = kwargs.get("sport", "")
+        self.gender = kwargs.get("gender", "")
+        self.date_time = kwargs.get("date_time", "")
+        self.location =kwargs.get("location", "")
+        self.teams =kwargs.get("date_time", ("",""))
+        self.num_tickets = kwargs.get("num_tickets", 0)
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    # Link to the Game table using assocaiton table 
+    past_games = db.relationship("Game", secondary=game_user_association, back_populates="users_attending")
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a user object 
+        """
+        self.username = kwargs.get("username", "")
+        self.email = kwargs.get("email", "")
+        self.balance = kwargs.get("balance", 0)
+
+
