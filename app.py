731821e482402701e998818a54782030ba9e71b2
cejiogu <chimdije@gmail.com>
2023-11-28 01:13:23 -0500

    Add app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+
+from db import Game
+from db import User
+from db import Ticket
+from db import db
+
+from flask import request
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+db_filename = "games.db" # Come back to
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+# Generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    return json.dumps({"error":message}), code
+
