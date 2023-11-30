c57cabb15be3620745d85d29e4d0e5f4866c055b
cejiogu <chimdije@gmail.com>
2023-11-30 18:44:41 -0500

    Add home and away team columns to Game Model, remove default value from object initialization

--- src/db.py
+++ src/db.py
@@ -19,10 +19,15 @@ class Game(db.Model):
     location = db.Column(db.String, nullable=False)
     date_time = db.Column(db.DateTime, nullable=False)
 
-    """
-    PickleType allows us to use tuple as dattpe (automatically serializes and deserializaes for databse) 
-    """
-    teams = db.Column(db.PickleType)
+    
+    # """
+    # PickleType allows us to use tuple as datatpe (automatically serializes and deserializaes for databse) 
+    # """
+    # teams = db.Column(db.PickleType)
+    # XXX: May not be best method of storing this information
+
+    home_team = db.Column(db.String, nullable=False)
+    away_team = db.Column(db.String, nullable=False)
     num_tickets = db.Column(db.Integer, nullable=False)
     sold_out = db.Column(db.Boolean, nullable=False, default=False)
     tickets = db.relationship("Ticket", cascade="delete")
@@ -34,11 +39,12 @@ class Game(db.Model):
         """
         Initialize a Game Object 
         """
-        self.sport = kwargs.get("sport", "")
-        self.sex = kwargs.get("sex", "")
-        self.date_time = kwargs.get("date_time", "")
-        self.location =kwargs.get("location", "")
-        self.teams = kwargs.get("teams", ("",""))
+        self.sport = kwargs.get("sport")
+        self.sex = kwargs.get("sex")
+        self.date_time = kwargs.get("date_time")
+        self.location =kwargs.get("location") # No need to include default value, throw error instead
+        self.home_team = kwargs.get("home_team")
+        self.away_team = kwargs.get("away_team")
         self.num_tickets = kwargs.get("num_tickets") # When initializing a game, the amount of tickets remaining should never be 0 (there would be no attendees)
 
     def serialize(self):
@@ -53,7 +59,8 @@ class Game(db.Model):
             # Converting date type object in database to stirng format to serialize 
             "date_time": self.date_time.strftime('%Y-%m-%d %H:%M:%S'),
             "location": self.location,
-            "teams": self.teams,
+            "home_team": self.home_team,
+            "away_team": self.away_team,
             "num_tickets": self.num_tickets,
             "tickets":  [t.simple_serialize() for t in self.tickets],
             "users_attending":  [u.simple_serialize() for u in self.users_attending]
