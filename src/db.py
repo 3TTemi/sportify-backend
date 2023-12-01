865c9e092defcf37d8c26b0934cec2efb7026a6f
cejiogu <chimdije@gmail.com>
2023-11-30 19:26:53 -0500

    Add first and last name columns to User table

--- src/db.py
+++ src/db.py
@@ -69,6 +69,8 @@ class Game(db.Model):
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
